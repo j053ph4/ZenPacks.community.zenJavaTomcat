9d9026a0becfac401a37be1191da0f76236f265e
Joseph Anderson <janderson@agero.com>
2015-03-31 08:50:19 -0500

    changed license to Apache

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 NAME = "ZenPacks.community.zenJavaTomcat"
 VERSION = "1.0.0"
 AUTHOR = "Joseph Anderson"
-LICENSE = "GPLv2+"
+LICENSE = "Apache"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
 PACKAGES = ['ZenPacks', 'ZenPacks.community', 'ZenPacks.community.zenJavaTomcat']
 INSTALL_REQUIRES = ['ZenPacks.community.zenJavaApp>=3.0', 'ZenPacks.community.ConstructionKit>=3.0']
